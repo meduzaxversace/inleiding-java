61e7a739f69534ea681c18601648c2ce6401bc70
meduzaxversace <2098064@talnet.nl>
2019-09-03 10:38:02 +0200

    WelcomeBACK

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -3,9 +3,8 @@ package h01;
 /**
  * Created by piet on 31-08-16.
  */
-public class HalloRocDev {
+    public class HalloRocDev {
 
-    public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+    public static void main(String[] args) { System.out.println("Welcome back Meduza!");
     }
 }
