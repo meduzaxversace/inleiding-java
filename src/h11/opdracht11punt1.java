90063e73cd52374d137bfa0ee81534541b8066cb
guccixversace <2098064@talnet.nl>
2019-10-17 11:20:58 +0200

    .

---
+++ src/h11/opdracht11punt1.java
@@ -0,0 +1,9 @@
+package h11;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class opdracht11punt1 extends Applet {
+
+}
---
+++ src/h14/opdracht14punt1.java
@@ -0,0 +1,4 @@
+package h14;
+
+public class opdracht14punt1 {
+}
